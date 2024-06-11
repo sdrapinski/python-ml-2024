108698d6d640b5d8fbe2bb4d46f0647bea8fb397
sdrapinski <szymondrapinski@gmail.com>
2024-06-11 03:25:19 +0200

    basics1

---
+++ basics/basics.py
@@ -0,0 +1,15 @@
+#type conversion
+
+print(type(str(5)))
+#escape sequence
+weather= 'it\'s kinda sunny \n new line'
+#print(weather)
+
+name ="john"
+surname="dohn"
+# print(f"Hi {name} {surname}")
+
+#string indexing
+indexing = "0123456789"
+
+print(indexing[::-1]) #reverse
